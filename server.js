ffab35bdef118ab835439973eda5f96779e8caf6
Your Name <you@example.com>
2022-03-17 19:14:08 +0800

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const server = require('https').createServer(app);
+const server = require('http').createServer(app);
 const io = socketIO(server);
 shared.io = io;
 
